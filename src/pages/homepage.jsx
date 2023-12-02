8fe620179506c395ae2fae5f772d766ef9d5489a
Samer Charaf Eddine <samer.charaf3@gmail.com>
2023-12-02 18:50:45 +0200

    init commit

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <section>
+      <div>
+        <p>this this a test</p>
+      </div>
+    </section>
+  );
+};
+
+export default Homepage;
